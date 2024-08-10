f39267e97fcd7d677a291c429545ec8df56c5e1b
Pedro Santos <peusantos@gmail.com>
2024-08-10 16:04:41 -0300

    feat: Adds Transfer strategies

---
+++ app-modules/Wallet/Services/CommonToMerchantTransferStrategy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppModules\Wallet\Services;
+
+use AppModules\User\Models\User;
+use AppModules\Wallet\Services\Interfaces\TransferServiceInterface;
+use AppModules\Wallet\Services\Interfaces\BalanceValidatorInterface;
+use AppModules\Wallet\Repositories\Interfaces\WalletRepositoryInterface;
+
+class CommonToMerchantTransferStrategy implements TransferServiceInterface
+{
+
+
+    public function __construct(
+        protected WalletRepositoryInterface $walletRepository,
+        protected BalanceValidatorInterface $balanceValidator
+    ) {}
+
+    public function transfer(User $sender, User $receiver, float $amount): bool
+    {
+        if (!$this->balanceValidator->validate($sender->id, $amount)) {
+            return false;
+        }
+
+        $this->walletRepository->decreaseBalance($sender->id, $amount);
+        $this->walletRepository->increaseBalance($receiver->id, $amount);
+
+        return true;
+    }
+}
