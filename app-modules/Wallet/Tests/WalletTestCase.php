0813d92cb7e34aab54d9a8a568dee3a3015d2051
Pedro Santos <peusantos@gmail.com>
2024-08-10 16:06:03 -0300

    test: Adds unit and feature tests.

---
+++ app-modules/Wallet/Tests/WalletTestCase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppModules\Wallet\Tests;
+
+use Tests\TestCase;
+use AppModules\User\Models\User;
+use AppModules\Wallet\Models\Wallet;
+use AppModules\User\Models\CommonUser;
+use AppModules\User\Models\MerchantUser;
+
+class WalletTestCase extends TestCase
+{
+    private static $emailCounter = 0;
+    private static $cpfCounter = 10000000000;
+    private static $cnpjCounter = 10000000000000;
+
+    public function createUser($type = 'common')
+    {
+        self::$emailCounter++;
+        $email = 'test' . self::$emailCounter . '@example.com';
+
+        if ($type === 'common') {
+            $user = new CommonUser();
+            $user->cpf = self::$cpfCounter++;
+        } else {
+            $user = new MerchantUser();
+            $user->cnpj = self::$cnpjCounter++;
+        }
+
+        $user->name = 'Test User';
+        $user->email = $email;
+        $user->password = bcrypt('password');
+        $user->save();
+
+        return $user;
+    }
+
+    public function createWallet($userId, $balance = 100)
+    {
+        $wallet = new Wallet();
+        $wallet->user_id = $userId;
+        $wallet->balance = $balance;
+        $wallet->save();
+
+        return $wallet;
+    }
+
+    public function createCommonUser()
+    {
+        return $this->createUser('common');
+    }
+
+    public function createMerchantUser()
+    {
+        return $this->createUser('merchant');
+    }
+}
