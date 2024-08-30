c9d1cd9fc154b3526d51ec57f1de5420982eeecd
Pedro Santos <peusantos@gmail.com>
2024-08-30 12:29:37 -0300

    test: Removes not used test cases

--- app-modules/Wallet/Tests/Unit/WalletTest.php
+++ app-modules/Wallet/Tests/Unit/WalletTest.php
@@ -9,8 +9,6 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WalletTest extends WalletTestCase
 {
-    use RefreshDatabase;
-
     public function test_wallet_belongs_to_user()
     {
         $user = $this->createUser();
@@ -18,34 +16,5 @@ class WalletTest extends WalletTestCase
 
         $this->assertInstanceOf(User::class, $wallet->user);
         $this->assertEquals($user->id, $wallet->user->id);
-    }
-
-    public function test_it_can_increase_balance()
-    {
-        $user = $this->createUser();
-        $wallet = $this->createWallet($user->id, 100);
-
-        $wallet->increaseBalance(50);
-
-        $this->assertEquals(150, $wallet->balance);
-    }
-
-    public function test_it_can_decrease_balance()
-    {
-        $user = $this->createUser();
-        $wallet = $this->createWallet($user->id, 100);
-
-        $wallet->decreaseBalance(40);
-
-        $this->assertEquals(60, $wallet->balance);
-    }
-
-    public function test_it_checks_sufficient_balance()
-    {
-        $user = $this->createUser();
-        $wallet = $this->createWallet($user->id, 100);
-
-        $this->assertTrue($wallet->hasSufficientBalance(50));
-        $this->assertFalse($wallet->hasSufficientBalance(150));
     }
 }
